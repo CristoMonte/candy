81c7a01257f947e2924deadf000ad901c1544212
qiujuan <18279185327@163.com>
2018-04-04 16:19:50 +0800

    add new.vue

---
+++ src/views/new.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>this file for testing is merge need commit when create a new file on master</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
